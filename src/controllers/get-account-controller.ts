451a756658a9eb3e67881c83b12f4534a6dd141c
bacate <igabriel.oliveira@gmail.com>
2025-12-22 15:43:10 -0300

    database integration complete

--- src/controllers/get-account-controller.ts
+++ src/controllers/get-account-controller.ts
@@ -1,11 +1,11 @@
 import http from 'http'
 import { getAccountService } from "../services/get-account-service"
 import { StatusCode } from '../utils/status-code'
+import { Account } from '../models/account-model'
 
 export const getAccountController = (async (request: http.IncomingMessage, response: http.ServerResponse) => {
 
-    //needed typing
-    const content = await getAccountService()
+    const content: Account[] = await getAccountService()
 
     response.writeHead(StatusCode.OK, { 'Content-Type': 'aplication/json' })
     response.write(JSON.stringify(content))
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Account } from "../models/account-model";
 
-mongoose.connect("mongodb://localhost:27017/finbridge").then(() => {
+mongoose.connect("mongodb://localhost:27017/FinBridge").then(() => {
     console.log("Database is connected")
 }).catch((error) => console.log(error))
 
@@ -12,7 +12,7 @@ const accountSchema = new mongoose.Schema({
     fromBank: String
 })
 
-const AccountModel = mongoose.model("Accounts", accountSchema)
+const AccountModel = mongoose.model("accounts", accountSchema)
 
 export const accountData = async (): Promise<Account[]> => {
     return await AccountModel.find()
